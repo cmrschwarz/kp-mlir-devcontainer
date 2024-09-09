beda5214a5091a13f6996fbc835567e96cab4ffb
Christian Schwarz <cmrs@cmrs.io>
2024-09-09 16:05:07 +0200

    add devcontainer setup

---
+++ .devcontainer/bash_profile.sh
@@ -0,0 +1,8 @@
+export PATH="$PATH:$HOME/just:/usr/lib/llvm-14/bin/"
+
+export WORKSPACE="$HOME/kp-mlir"
+
+export LLVM_BUILD_TYPE="Debug"
+
+export LLVM_INSTALL_DIR="$HOME/llvm-project"
+export LLVM_BUILD_DIR="$HOME/llvm-project/build-$LLVM_BUILD_TYPE/"
